a2a9fdc2d5e6e3d8e74b85a6523ea87d69df9004
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2025-03-17 17:27:04 +0100

    Changes in config in vercel and vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   server: {
     open: true,
   },
-});
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+})
