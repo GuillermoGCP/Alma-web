705d90593a35250cd8694c8a150313642f78ac6f
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2025-03-17 17:32:20 +0100

    ...

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-  },
   server: {
     open: true,
-    historyApiFallback: true, // Asegura que las rutas funcionan con React Router
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
   },
 })
