1a6a269174084f042611d9e048454603793fc132
GuillermoGCP <guillermocporto@gmail.com>
2025-09-18 23:41:56 +0200

    dinahosting deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // estoy sirviendo en https://almalactancia.org/
   server: {
-    open: true,
+    open: true, // solo en dev
+    port: 5173,
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true,
   },
 })
